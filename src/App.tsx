f42079e3981d3f52280f607136da4e829c898b12
daipengpeng <16602124116@163.com>
2021-03-11 22:43:17 +0800

    init
    init 项目

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import Button from './components/Button/Button'
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      111112
+    <div>
+      <Button type="primary" plain>默认按钮</Button>
+      <Button type="primary" plainest>默认按钮</Button>
+      <Button size="medium">中等按钮</Button>
+      <Button size="small">小型按钮</Button>
+      <Button size="mini">超小按钮</Button>
+      <Button type="success">默认按钮</Button>
+      <Button type="danger">中等按钮</Button>
+      <Button type="warning">小型按钮</Button>
+      <Button type="info" plain>超小按钮</Button>
     </div>
   );
 }
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,8 +1,36 @@
 import React from 'react'
-const Button: React.FC = () => {
+import classnames from 'classnames'
+
+interface BaseButtonProps {
+  size?: String,
+  plain?: Boolean,
+  plainest?: Boolean,
+  disabled?: Boolean,
+  type?: String,
+  loading?: Boolean,
+  circle?: Boolean,
+  round?: Boolean,
+  icon?: String,
+  children?: React.ReactNode
+}
+
+const Button: React.FC<BaseButtonProps> = (props: BaseButtonProps) => {
+  const {size,plain,plainest,disabled,type,round,children} = props
+  const btnClass = classnames('btn', {
+    [`btn-${type}`]: type,
+    [`btn-${size}`]: size,
+    [`btn-plain`]: plain,
+    [`btn-plainest`]: plainest,
+    [`btn-${disabled}`]: disabled,
+    [`btn-${round}`]: round
+  })
   return (
-    <div>111</div>
+    <button className={btnClass}>{children}</button>
   )
 }
+
+Button.defaultProps = {
+  type: 'default'
+};
 
 export default Button
