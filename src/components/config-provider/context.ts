6f67903450efeb85beacfa15cf4283a1b895c623
daipengpeng <16602124116@163.com>
2021-03-13 18:47:51 +0800

    feat: 完善Button组件

---
+++ src/components/config-provider/context.ts
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export interface ConfigConsumerProps {
+  getPrefixClass: (suffix? :string, custom?: string) => string
+}
+
+const getPrefixClasss = (suffix?: string, custom?: string) => {
+  if (custom) return custom;
+
+  return suffix ? `tracy-${suffix}` : 'tracy';
+};
+
+export const ConfigContext = React.createContext<ConfigConsumerProps>({
+  getPrefixClass: getPrefixClasss,
+});
