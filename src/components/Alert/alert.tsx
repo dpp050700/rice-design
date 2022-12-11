83bef401a7630f4702152292f7327defc30f4e3e
戴鹏鹏 <16602124116@163.com>
2022-12-11 22:12:22 +0800

    Alert: 编写 Alert 组件

--- src/components/Alert/alert.tsx
+++ src/components/Alert/alert.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import classnames from 'classnames';
 
+export type AlertType = 'primary' | 'danger' | 'warning' | 'info';
+
 interface AlertProps {
   className?: string;
+  title: React.ReactNode;
+  description: React.ReactNode;
+  type?: AlertType;
+  closeable?: boolean;
+  plain?: boolean;
+  showIcon?: boolean;
 }
 
 const Alert: React.FC<AlertProps> = (props) => {
