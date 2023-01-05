24f7b63a307aa7a586e15f9845d57cd3c7c16e7a
戴鹏鹏 <16602124116@163.com>
2023-01-05 16:31:08 +0800

    Notification: 编写 Notification 组件

--- src/components/Notification/NotificationUI.tsx
+++ src/components/Notification/NotificationUI.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { NotificationType } from './interface';
+import classnames from 'classnames';
 
 interface NotificationUIProps {
   prefixCls?: string;
@@ -11,9 +12,12 @@ interface NotificationUIProps {
 }
 
 const NotificationUI: React.FC<NotificationUIProps> = (props) => {
-  const { prefixCls, title, description } = props;
+  const { prefixCls, title, type, description } = props;
+  const cls = classnames(`${prefixCls}-notify`, {
+    [`${prefixCls}-notify--${type}`]: type
+  });
   return (
-    <div>
+    <div className={cls}>
       <div className={`${prefixCls}-notify-title`}>{title}</div>
       <div className={`${prefixCls}-notify-description`}>{description}</div>
     </div>
